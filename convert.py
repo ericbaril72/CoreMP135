cd989764af5811351efede3caace555890176b5f
EricB <eric@baril.com>
2025-03-13 09:46:59 -0400

    Converts word arrays to byte arrays

--- convert.py
+++ convert.py
@@ -1,10 +1,25 @@
-# converting imagesw from this URL: https://notisrac.github.io/FileToCArray/
+# To convert a Picture ( BMP/jpg/png ) to "C" code structure
 #
+# 1. Online Converter to .c file
+# URL: https://notisrac.github.io/FileToCArray/
+# Result is encoded in 16-bits
+#
+# This script converts online converter result arrays in the form of
+#       0xffff, 0x1234, 0xffff, 0xabcd, ...
+#
+#  to
+#       0xff, 0xff, 0x12, 0x34, 0xff, 0xff, 0xab, axcd, ....
 #
 
-outfile=open("./cmd_show_logo.h","w")
-outfile.write('uint8_t eb_custom_image[] = {')
-with open("./bootpicpng.h") as infile:
+
+SRC_H_FILENAME     = "./bootpicpng.h"
+TARGET_H_FILENAME  = "./cmd_show_logo.h"
+IMG_VARNAME        = 'uint8_t eb_custom_image[]'
+
+outfile=open(TARGET_H_FILENAME,"w")
+outfile.write(IMG_VARNAME+' = {')
+
+with open(SRC_H_FILENAME) as infile:
     cnt=0
     isdata=False
     newline=''
@@ -14,23 +29,14 @@ with open("./bootpicpng.h") as infile:
         if '0x' in line:
             values=line.split(',')
             newline+="\n      "
-            #
-            #print(values[:16])
-            #print(values[-16:])
-            #print("len:",len(values))
+            
             for i in range(0,int(   (len(values))/2)):
                 if '0x' in values[i]:
                     value=int(values[i],16)
                     bytes_value='0x{:02X}, 0x{:02X}, '.format(value>>8,value&0xff)
                     newline +=bytes_value
                     
-            if cnt<10:
-                print(cnt,line)
-            
-            #print("to:",newline)
-            #print("len:",len(newline.split(",")))
             outfile.write(newline[:-2])
             newline=','
-            
-            
+
 outfile.write('\n};\n')
